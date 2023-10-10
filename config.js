f449bf1489e1855fc621948484bdec46f6529a43
Cristhian <79179637+Buddier@users.noreply.github.com>
2023-10-10 13:17:04 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    botClientID: "YOUR_BOT_ID",
+    botPrefix: "!",
+    ownerID: "YOUR_ID",
+    embedColor: "#00dbff",
+    embedfooterText: "Discord.js v13 Example by Expectatives#1157"
+}
